5ef41a87b305ef482cc08534c618beae406483cc
strobofactory <info@strobofactory.net>
2026-08-16 20:52:35 +0900

    Delete hapiveri-app directory

--- hapiveri-app/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from 'next';
-
-const config: NextConfig = {
-  images: {
-    remotePatterns: [{ protocol: 'https', hostname: 'cdn.shopify.com' }],
-  },
-};
-
-export default config;
